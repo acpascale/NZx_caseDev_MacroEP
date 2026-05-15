4bf685fb94ac1fbcf14692ac75adae94b3a4eebf
Andrew Pascale <acpascale@gmail.com>
2026-05-15 15:56:49 +1000

    2p dev to stable

--- electricity_fiveZ_8736_2p_nziShadow_dev/run.jl
+++
@@ -1,9 +0,0 @@
-using MacroEnergy
-using Gurobi
-using Logging
-
-(system, model) = run_case(@__DIR__; 
-                    optimizer=Gurobi.Optimizer, log_level=Logging.Debug,
-                    optimizer_attributes=("Method" => 2, "Crossover" => 1, "BarConvTol" => 1e-4),
-    lazy_load=false,
-);
